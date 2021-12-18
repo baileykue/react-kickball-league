323b3e4b61b7bb42d6b8c018654991fff940c3c5
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-12-17 17:25:02 -0800

    re writing player fetch function, as well as get playr by id function

--- src/services/players.js
+++ src/services/players.js
@@ -1,10 +1,11 @@
+import { checkError, client } from './client';
+
 export async function fetchPlayers() {
-  const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/players?`, {
-    headers: {
-      apikey: process.env.REACT_APP_SUPABASE_KEY,
-      Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
-    },
-  });
-  const data = await resp.json();
-  return data;
+  const response = await client.from('players').select();
+  return checkError(response);
+}
+
+export async function getPlayerById(id) {
+  const response = await client.from('players').select('*').match({ id });
+  return checkError(response);
 }
