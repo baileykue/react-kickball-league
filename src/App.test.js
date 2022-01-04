833389310788280d7cfff1d1fa122d4ec69368be
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-03 16:53:35 -0800

    fixing the home js and app js test i hope

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Home/i);
+  const linkElement = screen.getByText(/This is the Home Page/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/views/Home/Home.test.js
+++ src/views/Home/Home.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Home from './Home';
 
-test('checks for main component text', () => {
+test('checks for home component text', () => {
   render(<Home />);
   const header = screen.getByText(/This is the Home Page/i);
   expect(header).toBeInTheDocument();
